4d36e3eb8af27ce60601cb8c9d63524c97d2bd1c
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-10-15 20:36:52 -0500

    All done, except closure stretch attempt.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,24 +41,35 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, console.log);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, console.log);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(104, 295, console.log);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(3000, 2, console.log);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+contains("yo-yo", items, console.log);
+contains("backpack", items, console.log);
 
 /* STRETCH PROBLEM */
 
@@ -66,4 +77,14 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const noDupes = [];
+  noDupes.push("Pencil", "yo-yo", "eraser");
+  console.log(noDupes);
+  array.forEach(element => {
+    if (noDupes.indexOf(element) === -1) {
+      noDupes.push(element);
+    }
+  });
+  return cb(noDupes);
 }
+removeDuplicates(items, console.log);
